295727ce3c2ecf184e46f911aed32353e2be29f7
Tom <t-lauth@web.de>
2022-03-01 12:00:56 +0100

    creating new files

---
+++ app/src/main/java/com/example/ichhabschonmal/Score.java
@@ -0,0 +1,14 @@
+package com.example.ichhabschonmal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Score extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rules);
+    }
+}
