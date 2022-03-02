df5f573d7dc45e8a34c836050abe85dc3d76718c
dominik <sauterdominik1211@gmail.com>
2022-03-02 15:36:04 +0100

    Added a RecyclerView for and implemented classes to view all Stories

--- app/src/main/java/com/example/ichhabschonmal/LoadGame.java
+++ app/src/main/java/com/example/ichhabschonmal/LoadGame.java
@@ -1,8 +1,12 @@
 package com.example.ichhabschonmal;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 public class LoadGame extends AppCompatActivity {
 
@@ -10,6 +14,16 @@ public class LoadGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.load_game);
+
+
+        AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "database").allowMainThreadQueries().build();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
 
 
 
