f1dfef1261295798aa2df3b4339d731a0fe2e9c1
dominik <sauterdominik1211@gmail.com>
2022-09-19 16:32:55 +0200

    Changed design of activity_main.xml

--- app/src/main/java/com/example/ichhabschonmal/NewGame.java
+++ app/src/main/java/com/example/ichhabschonmal/NewGame.java
@@ -141,7 +141,7 @@ public class NewGame extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
 
         // showing the back button in action bar
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        //actionBar.setDisplayHomeAsUpEnabled(true);
 
     }
 
